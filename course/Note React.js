34a141bdc568b0485b012631d906d5503d8f2b73
lifeng jiang <henry@lifengs-Air.lan>
2021-03-10 16:02:50 +1100

    create react comps

---
+++ course/Note React.js
@@ -0,0 +1,17 @@
+Note React.js
+
+3/10
+
+JSX => allow me to use HTML tag in js files
+
+Step to create a react app 
+1. modify app.js
+export app
+
+2. in index.js
+import app
+use ReactDom.render(1.app 2. pos on html) 
+
+
+JSX 
+1. only have 1 root element 
